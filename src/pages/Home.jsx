237f47d2e6a31abad9f616c11d3baa0a56d14809
Archit <architojha420@gmail.com>
2024-01-31 02:58:36 +0530

    scanner implemented

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,42 @@
+import React, { useState } from "react";
 import Typography from "@mui/material/Typography";
 import {
   AuthenticatedTemplate,
   UnauthenticatedTemplate,
 } from "@azure/msal-react";
+import QrReader from "react-qr-scanner";
+import { Link as RouterLink } from "react-router-dom";
 
 export const Home = () => {
+  const [scanResult, setScanResult] = useState("Hold QR Steady");
+
+  const handleScan = (result) => {
+    if (result) {
+      setScanResult(result);
+      console.log(result)
+
+
+    }
+  };
+
+  const handleError = (error) => {
+    console.error(error);
+  };
+
   return (
     <>
       <AuthenticatedTemplate>
-        <Typography variant="h6">
-          You are signed-in. Select profile to call Microsoft Graph.
-        </Typography>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <Typography variant="h6">
+            You are signed-in. Select profile to call Microsoft Graph.
+          </Typography>
+          <QrReader
+            delay={100}
+            style={{ width: "100%" }}
+            onError={handleError}
+            onScan={handleScan}
+          />
+        </div>
       </AuthenticatedTemplate>
 
       <UnauthenticatedTemplate>
